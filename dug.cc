1ad4a90659d82ce7b0f4d3de05db097b9c01b1ff
Dezmon Fernandez <defernan@mines.edu>
2015-11-04 02:52:30 -0700

    printing record, next step recursion

--- dug.cc
+++ dug.cc
@@ -102,48 +102,104 @@ void readDNSResponse(unsigned char* buffer, unsigned char* questionName){
     
     header = (DNSHeader*)buffer;
     parser = &buffer[ sizeof(DNSHeader) + strlen((const char*)questionName) + NULL_CHAR_SIZE + sizeof(DNSQueryInfo) ];
-    vector<DNSResourceRecord> answers;
+    vector<DNSResourceRecord> answers, auth, addl;
     cout << ntohs(header->qdCount) << " questions\n";
-    cout << ntohs(header->anCount) << " answers\n";
-    cout << ntohs(header->nsCount) << " ns\n";
-    cout << ntohs(header->arCount) << " addln\n";
-    
-    //answers
-    for(int i = 0; i < ntohs(header->anCount); i++){
-        DNSResourceRecord record;
-        record.name = readName(buffer, parser, &octetsMoved);
-        parser += octetsMoved;
-        unsigned char* lname =  record.name; 
-        cout << "Name " << lname << endl;
-        record.resourceInfo = (DNSResourceInfo*)parser; 
-        //cout << "\nRES " << sizeof(DNSResourceInfo) << endl; 
-        parser+= sizeof(DNSResourceInfo);
-        //if type A 
-        //cout << ntohs(record.resourceInfo->type);
-        if(ntohs(record.resourceInfo->type) == 1){
-            record.rdata = (unsigned char*)malloc(ntohs(record.resourceInfo->rdLength));
-            for(int j = 0; j < ntohs(record.resourceInfo->rdLength); j++){
-                record.rdata[j] = parser[j];
-            }
-            record.rdata[ntohs(record.resourceInfo->rdLength)] = 0x00;
-            parser += ntohs(record.resourceInfo->rdLength);
-            //used for printing 
-            struct sockaddr_in a;
-            long *p;
-            p=(long*)record.rdata;
-            a.sin_addr.s_addr=(*p); //working without ntohl
-            printf("has IPv4 address : %s\n",inet_ntoa(a.sin_addr));
-
-        }else{// if CName
-            record.rdata = readName(buffer, parser, &octetsMoved);
+    //IF A RETURN, else RECURSIVELYSEARCH
+    if( ntohs(header->anCount) > 0 ){
+        //answers
+        cout <<  ntohs(header->anCount) << " ANSWERS\n";
+        for(int i = 0; i < ntohs(header->anCount); i++){
+            DNSResourceRecord record;
+            record.name = readName(buffer, parser, &octetsMoved);
             parser += octetsMoved;
-            unsigned char* n = record.rdata;
-            cout << "CName " << n << endl;
-        }
-        cout << endl;
-    }
+            unsigned char* lname =  record.name; 
+            cout << "Name " << lname << endl;
+            record.resourceInfo = (DNSResourceInfo*)parser; 
+            //cout << "\nRES " << sizeof(DNSResourceInfo) << endl; 
+            parser+= sizeof(DNSResourceInfo);
+            //if type A 
+            //cout << ntohs(record.resourceInfo->type);
+            if(ntohs(record.resourceInfo->type) == 1){
+                record.rdata = (unsigned char*)malloc(ntohs(record.resourceInfo->rdLength));
+                for(int j = 0; j < ntohs(record.resourceInfo->rdLength); j++){
+                    record.rdata[j] = parser[j];
+                }
+                record.rdata[ntohs(record.resourceInfo->rdLength)] = 0x00;
+                parser += ntohs(record.resourceInfo->rdLength);
+                //used for printing 
+                struct sockaddr_in a;
+                long *p;
+                p=(long*)record.rdata;
+                a.sin_addr.s_addr=(*p); //working without ntohl
+                printf("has IPv4 address : %s\n",inet_ntoa(a.sin_addr));
 
+            }else{// if CName
+                record.rdata = readName(buffer, parser, &octetsMoved);
+                parser += octetsMoved;
+                cout << "CName " << record.rdata << endl;
+            }
+            cout << endl;
+            answers.push_back(record);
+        }
+    }else if( ntohs(header->nsCount) ) { 
+        cout << ntohs(header->nsCount) <<  " NS\n";
+        for(int i = 0; i < ntohs(header->nsCount); i++){
+            DNSResourceRecord record;
+            record.name = readName(buffer, parser, &octetsMoved);
+            parser += octetsMoved;
+
+            record.resourceInfo = (DNSResourceInfo*)parser;
+            parser+= sizeof(DNSResourceInfo);
+
+            record.rdata = readName(buffer, parser, &octetsMoved);
+            parser+= octetsMoved;
+            cout << "NS name  " << record.rdata << endl;
+            auth.push_back(record);
+        }
+
+        cout << ntohs(header->arCount) << " addln\n";
+        //for(int i = 0; i < ntohs(header->arCount); i++){
+        for(int i = 0; i < ntohs(header->nsCount); i++){
+            DNSResourceRecord record;
+
+            record.name=readName(buffer, parser, &octetsMoved);
+            parser += octetsMoved;
+
+            record.resourceInfo=(DNSResourceInfo*)parser;
+            parser += sizeof(DNSResourceInfo);
+            cout << record.name;
+
+            if(ntohs(record.resourceInfo->type) == 1 ){
+                record.rdata = (unsigned char*)malloc(ntohs(record.resourceInfo->rdLength));
+                for(int i = 0; i < ntohs(record.resourceInfo->rdLength); i++){
+                    record.rdata[i]=parser[i];
+
+                }
+                record.rdata[ntohs(record.resourceInfo->rdLength)] = 0x00;
+                parser+=ntohs(record.resourceInfo->rdLength);
+
+                struct sockaddr_in a;
+                long *p;
+                p=(long*)record.rdata;
+                a.sin_addr.s_addr=(*p); //working without ntohl
+                printf(" has IPv4 address : %s\n",inet_ntoa(a.sin_addr));
+
+            }
+            else{
+                cout << "IM GETTING CALLED";
+                record.rdata=readName(buffer, parser, &octetsMoved);
+                parser += octetsMoved;
+            }
+
+           addl.push_back(record);
+        }
+    }else{
+
+        cout << "NO ANSWERS\n";
+    }
+    return;
 }
+
 void makeDNSQuery(unsigned char* host, const char* serverIP, int clientSocket){
     unsigned char buffer[MAX_MESSAGE_SIZE]; 
     unsigned char* questionName;
@@ -270,239 +326,4 @@ int main(int argc, char **argv) {
     close(listenfd);
     */
 }
-
-/*
-//get filesize in bytes
-int getFileSize(string fileName){
-    int fileSize = -1; 
-    struct stat buff;
-    //fill in stat structure and retrieve file size 
-    int statReturn = stat(fileName.c_str(), &buff);
-    if(statReturn == 0){
-        return buff.st_size;
-    }
-
-    return fileSize; 
-}
-//get content type, should fix but this will always be last field in response
-char* getContentType(string fileName){
-    char contentType[MAXLINE];
-    if((fileName.find("jpeg") != std::string::npos) || (fileName.find("jpg") != std::string::npos) ){
-        strcpy(contentType, "\r\nContent-Type: image/jpeg\r\n\r\n");
-    }else{
-        strcpy(contentType , "\r\nContent-Type: text/html\r\n\r\n");
-    }
-     
-    return contentType;
-}
-// ***************************************************************************
-// * readGetRequest()
-// *  This function should read each line of the request sent by the client
-// *  and check to see if it contains a GET request (which is the part  we are
-// *  interested in).  You know you have read all the lines when you read
-// *  a blank line.
-// ***************************************************************************
-string readGetRequest(int sockfd) {
-    char buff[MAXLINE];
-    int bytesRead;
-    while ( (bytesRead=read(sockfd, buff, MAXLINE)) > 0){
-        string getReq = string(buff).substr(0, bytesRead);
-
-        if(getReq.find("GET") != std::string::npos){
-            return getReq;
-        }
-    }
-    return "NO GET"; 
-}
-
-
-// ***************************************************************************
-// * parseGET()
-// *  Parse the GET request line and find the filename.  Since HTTP requests
-// *  should always be relitive to a particular directory (so you don't 
-// *  accidently expose the whole filesystem) you should prepend a path
-// *  as well. In this case prepend "." to make the request relitive to
-// *  the current directory. 
-// *
-// *  Note that a real webserver will include other protections to keep
-// *  requests from traversing up the path, including but not limited to
-// *  using chroot.  Since we can't do that you must remember that as long
-// *  as your program is running anyone who knows what port you are using
-// *  could get any of your files.
-// ***************************************************************************
-string parseGET(string getRequest) {
-   
-    //set stream = to string 
-    stringstream stream;
-    stream.str(getRequest);
-    string file = ".";
-    string fileLoc = "";
-    //string parse
-    while( stream >> fileLoc ){
-        if( fileLoc == "GET" ){
-            stream >> fileLoc;
-            break;
-        }
-    }
-    
-    //clear stream
-    stream.str(string());
-    stream.clear();
-
-    file = file + fileLoc;
-     
-    return file;
-
-}
-
-// ***************************************************************************
-// * fileExists()
-// *  Simple utility function I use to test to see if the file really
-// *  exists.  Probably would have been simpler just to put it inline.
-// ***************************************************************************
-bool fileExists(string filename) {
-    ifstream file(filename.c_str());
-    if(file){
-        file.close();
-        return true;
-    }
-    file.close();
-    return false;
-}
-
-// ***************************************************************************
-// * sendHeader()
-// *  Send the content type and rest of the header. For this assignment you
-// *  only have to do TXT, HTML and JPG, but you can do others if you want.
-// ***************************************************************************
-bool sendHeader(int sockfd, string fileName) {
-    //status line 
-    char statusLine[MAXLINE] = "\nHTTP/1.0 200 OK\r\n";
-    char contentLength[MAXLINE] = "Content-Length: ";
-    char fileSize[MAXLINE] ;
-    sprintf(fileSize, "%d", getFileSize(fileName));
-    //content type must be last, has return line and blank line to end header
-    char contentType[MAXLINE];
-    strcpy(contentType, getContentType(fileName));
-    
-    write(sockfd, statusLine, strlen(statusLine)); 
-    write(sockfd, contentLength, strlen(contentLength));
-    write(sockfd, fileSize, strlen(fileSize));
-    write(sockfd, contentType, strlen(contentType)); 
-    return true;
-}
-
-// ***************************************************************************
-// * sendFile(int sockfd,string filename)
-// *  Open the file, read it and send it.
-// ***************************************************************************
-bool sendFile(int sockfd,string filename) {
-    int file = open(filename.c_str(), O_RDONLY);
-    int bytesRead;
-    int written;
-    char buff[MAXLINE];
-     
-    while((bytesRead = read(file, buff, MAXLINE))> 0){
-        char* p = buff; 
-        while(bytesRead > 0){
-            written = write(sockfd, p, bytesRead);
-            bytesRead = bytesRead - written;
-            p += written;
-        }
-    }
-    close(file);
-    return true;
-}
-
-// ***************************************************************************
-// * send404(int sockfd)
-// *  Send the whole error page.  I can really say anything you like.
-// ***************************************************************************
-bool send404(int sockfd) {
-    char statusLine[MAXLINE] = "\nHTTP/1.0 404 NOT FOUND\r\n";
-    char contentLength[MAXLINE] = "Content-Length: 25\r\n";
-    char contentType[MAXLINE] = "Content-Type: text/html\r\n\r\n"; //insert blank line at end to end header
-    
-    
-    char message[MAXLINE] = "Requested file not found.";
-    
-    write(sockfd, statusLine, strlen(statusLine));
-    write(sockfd, contentLength, strlen(contentLength)); 
-    write(sockfd, contentType, strlen(contentType)); 
-
-    write(sockfd, message, strlen(message));
-
-    return true;
-}
-
-
-// ***************************************************************************
-// * processRequest()
-// *  Master function for processing thread.
-// *  !!! NOTE - the IOSTREAM library and the cout varibables may or may
-// *      not be thread safe depending on your system.  I use the cout
-// *      statments for debugging when I know there will be just one thread
-// *      but once you are processing multiple rquests it might cause problems.
-// ***************************************************************************
-void* processRequest(void *arg) {
-
-
-	// *******************************************************
-	// * This is a little bit of a cheat, but if you end up
-	// * with a FD of more than 64 bits you are in trouble
-	// *******************************************************
-	int sockfd = (long)arg;
-	if (DEBUG)
-		cout << "We are in the thread with fd = " << sockfd << endl;
-    
-    
-	// *******************************************************
-	// * Now we need to find the GET part of the request.
-	// *******************************************************
-	string getRequest = readGetRequest(sockfd);
-	if (DEBUG)
-		cout << "Get request is " << getRequest << endl;
-
-	// *******************************************************
-	// * Find the path/file part of the request.
-	// *******************************************************
-	string requestedFile = parseGET(getRequest);
-	if (DEBUG)
-		cout << "The file they want is " << requestedFile << endl;
-    // *******************************************************
-	// * Send the file
-	// *******************************************************
-	if (fileExists(requestedFile)) {
-		
-        // *******************************************************
-		// * Build & send the header.
-		// *******************************************************
-		sendHeader(sockfd, requestedFile);
-		if (DEBUG)
-			cout << "Header sent" << endl;
-
-		// *******************************************************
-		// * Send the file
-		// *******************************************************
-		sendFile(sockfd,requestedFile);
-		if (DEBUG)
-			cout << "File sent" << endl;
-
-	} else {
-		// *******************************************************
-		// * Send an error message 
-		// *******************************************************
-		if (DEBUG)
-			cout << "File " << requestedFile << " does not exist." << endl;
-		send404(sockfd);
-		if (DEBUG)
-			cout << "Error message sent." << endl;
-	}
-
-	if (DEBUG)
-		cout << "Thread terminating" << endl;
-    close(sockfd);
-}
-*/
-
 
